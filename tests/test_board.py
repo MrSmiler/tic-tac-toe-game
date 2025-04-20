14e4f55fb22e66d2570625adce9fe742a081fd45
Mohsen noei avval <noeyaval@mahsan.co>
2025-04-20 14:39:08 +0330

    feat: add board

--- tests/test_board.py
+++ tests/test_board.py
@@ -2,16 +2,36 @@ from tic_tac_toe_game.board import Board
 from tic_tac_toe_game.token import Token
 from tic_tac_toe_game.pos import Pos
 
-def test_set_single_token():
+
+class TestBoard():
 
     board = Board()
-    
-    pos: Pos = Pos(1, 1)
 
-    board.set_token(pos, Token.X)
+    def test_set_single_token(self):
 
-    assert board.get_token(pos) == Token.X
+        pos: Pos = Pos(1, 1)
 
-def test_set_single_token():
+        self.board.set_token(pos, Token.X)
 
-    pass
+        assert self.board.get_token(pos) == Token.X
+
+    def test_multiple_token(self):
+        pos1: Pos = Pos(1, 1)
+        pos2: Pos = Pos(2, 2)
+        pos3: Pos = Pos(3, 3)
+
+        self.board.set_token(pos1, Token.X)
+        self.board.set_token(pos2, Token.O)
+        self.board.set_token(pos3, Token.X)
+
+        assert self.board.get_token(pos1) == Token.X
+        assert self.board.get_token(pos2) == Token.O
+        assert self.board.get_token(pos3) == Token.X
+
+    def test_pos_out_of_boundary(self):
+
+        pos: Pos = Pos(10, 5)
+
+        self.board.set_token(pos, Token.X)
+
+        assert self.board.get_token(pos) == Token.X
--- tic_tac_toe_game/board.py
+++ tic_tac_toe_game/board.py
@@ -1,17 +1,29 @@
 from typing import List
 from tic_tac_toe_game.token import Token
 from tic_tac_toe_game.pos import Pos
+from dataclasses import dataclass
+from typing import ClassVar
 
+@dataclass
 class Board:
+    _board_rows: ClassVar[int] = 3
+    _board_cols: ClassVar[int] = 3
+
     tokens: List[Token]
 
     def __init__(self):
-
         self.tokens = [Token.NONE] * 9
-
     
+    def calc_index(self, pos: Pos) -> int:
+        array_pos_x = pos.x - 1
+        array_pos_y = pos.y - 1
+        index: int = (array_pos_x * self._board_rows) + array_pos_y
+        return index
+
     def set_token(self, pos: Pos, token: Token):
-        pass
+        index: int = self.calc_index(pos)
+        self.tokens[index] = token
 
     def get_token(self, pos: Pos) -> Token:
-        return Token.X
+        index: int = self.calc_index(pos)
+        return self.tokens[index]
